17620917b1d0f8f7dd34c77ece4a00a1b58d18fb
Ayman <aymoug@outlook.fr>
2025-01-01 20:32:42 +0000

    Début Italie

--- lib/italie.dart
+++ lib/italie.dart
@@ -16,6 +16,118 @@ class _ItalieState extends State<Italie> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+      body: Center(
+        child: Column( 
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/italie.png", width: 150, height: 85,),
+            const SizedBox(height: 20),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: const SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: Center(child: Text('Abarth')), 
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: const SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: Center(child: Text('Alfa Romeo')), 
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: const SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: Center(child: Text('Ferrari')), 
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: const SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: Center(child: Text('Fiat')), 
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: const SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: Center(child: Text('Lamborghini')), 
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: const SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: Center(child: Text('Lancia')), 
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: const SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: Center(child: Text('Maserati')), 
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: const SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: Center(child: Text('Pagani')), 
+                ),
+              ),
+            ),
+          ]
+        )
+      )
     );
   }
 }
