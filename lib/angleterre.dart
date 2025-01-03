6c6af16c56e11c6ef8af5fc54bb9fbac3225e49d
Ayman <aymoug@outlook.fr>
2025-01-03 19:41:32 +0000

    début uk

--- lib/angleterre.dart
+++ lib/angleterre.dart
@@ -16,6 +16,41 @@ class _AngleterreState extends State<Angleterre> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/uk.png",width: 150, height: 85,),
+            const SizedBox(height: 20,),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: const SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: Center(child: Text('Aston Martin')), 
+                ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {},
+                child: const SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: Center(child: Text('Bentley')), 
+                ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+          ],
+        ),
+      ),
     );
   }
 }
